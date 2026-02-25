9239591ad8a21ed489ee38420124086306559b6d
vinicius_porto <vinisepo@hotmail.com>
2026-02-25 15:16:12 -0300

    feat: adiciona mensagem no botão de informação"

--- app/src/main/java/com/caesperdidos/app/MainActivity.java
+++ app/src/main/java/com/caesperdidos/app/MainActivity.java
@@ -1,15 +1,32 @@
 package com.caesperdidos.app;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.activity.EdgeToEdge;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+  private MaterialButton btnInfo;
+
+    /// Mostra informações sobre o aplicativo
+  private void openMessageInfo(){
+          btnInfo = findViewById(R.id.btnInfo);
+
+      btnInfo.setOnClickListener(view -> {
+
+          AlertDialog.Builder alertDialog = new  AlertDialog.Builder(MainActivity.this);
+          alertDialog.setTitle("Sobre o aplicativo:");
+          alertDialog.setMessage("Objetivo: Localizar e resgatar cães perdidos pelo mundo");
+          alertDialog.show();
+
+      });
+  }
 
 
     @Override
@@ -17,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+
+        //Configura o botão para exibir a mensagem sobre o app
+        openMessageInfo();
+
 
 
 
