76efc1ee182bad79f3b30f810c651909d781404c
vinicius_porto <vinisepo@hotmail.com>
2026-03-22 16:15:54 -0300

    feat: adiciona HomeCadastroActivity e navegação Cria o arquivo HomeCadastroActivity Adiciona método para abrir a tela Atualiza o AndroidManifest com a activity

--- app/src/main/java/com/caesperdidos/app/HomeCadastroActivity.java
+++ app/src/main/java/com/caesperdidos/app/HomeCadastroActivity.java
@@ -1,6 +1,5 @@
 package com.caesperdidos.app;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,30 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.material.button.MaterialButton;
-
-public class HomeActivity extends AppCompatActivity {
-private MaterialButton btnCadastrarCachorros;
-
-private void openPageCadastro(){
-    btnCadastrarCachorros = findViewById(R.id.btnCadastrarCachorros);
-    btnCadastrarCachorros.setOnClickListener(view -> {
-        Intent intent = new Intent(HomeActivity.this, HomeCadastroActivity.class);
-        startActivity(intent);
-    });
-}
-
+public class HomeCadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
-
-
-        openPageCadastro();
-
-
+        setContentView(R.layout.activity_home_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
