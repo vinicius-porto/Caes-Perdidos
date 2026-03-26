e55eb5657182c94bd2ebec3910c29dce6bacaa6f
vinicius_porto <vinisepo@hotmail.com>
2026-03-25 22:15:19 -0300

    feat: adiciona classe CadastrarCachorro com validação de campos

---
+++ app/src/main/java/com/caesperdidos/app/CadastrarCachorro.java
@@ -0,0 +1,46 @@
+package com.caesperdidos.app;
+
+public class CadastrarCachorro {
+
+
+    private String nome;
+    private String raca;
+    private String cor;
+
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+
+
+}
--- app/src/main/java/com/caesperdidos/app/HomeCadastroActivity.java
+++ app/src/main/java/com/caesperdidos/app/HomeCadastroActivity.java
@@ -1,24 +1,132 @@
 package com.caesperdidos.app;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import androidx.appcompat.widget.AppCompatEditText;
+import com.google.android.material.button.MaterialButton;
 public class HomeCadastroActivity extends AppCompatActivity {
+
+
+    private MaterialButton btnCadastro;
+    private AppCompatEditText editNome,editRaca,editCor,editDescricao;
+
+
+    private void selectElements(){
+        btnCadastro = findViewById(R.id.btnCadastro);
+        editNome = findViewById(R.id.editNome);
+        editRaca = findViewById(R.id.editRaca);
+        editCor = findViewById(R.id.editCor);
+        editDescricao = findViewById(R.id.editDescricao);
+    }
+
+    private final String[] mensagensErro = {
+            "Por favor, insira o nome",
+            "Por favor, insira a raça",
+            "Por favor, insira a cor",
+            "Por favor, insira a descrição"
+    };
+
+
+    private void verificaCampos() {
+        String nome = editNome.getText().toString().trim();
+        String raca = editRaca.getText().toString().trim();
+        String cor = editCor.getText().toString().trim();
+        String descricao = editDescricao.getText().toString().trim();
+
+        //verifica os campos
+        if (nome.isEmpty() || raca.isEmpty() || cor.isEmpty() || descricao.isEmpty()) {
+
+            if (nome.isEmpty()) {
+                editNome.setError(mensagensErro[0]);
+                editNome.requestFocus();
+            } else if (raca.isEmpty()) {
+                editRaca.setError(mensagensErro[1]);
+                editRaca.requestFocus();
+            } else if (cor.isEmpty()) {
+                editCor.setError(mensagensErro[2]);
+                editCor.requestFocus();
+            } else if (descricao.isEmpty()) {
+                editDescricao.setError(mensagensErro[3]);
+                editDescricao.requestFocus();
+            }
+
+            return;
+        }
+
+        // Cria objeto
+        CadastrarCachorro cadastrarCachorro = new CadastrarCachorro();
+        cadastrarCachorro.setNome(nome);
+        cadastrarCachorro.setRaca(raca);
+        cadastrarCachorro.setCor(cor);
+        cadastrarCachorro.setDescricao(descricao);
+
+
+        Toast.makeText(this,
+                "Cachorro cadastrado:\n" +
+                        "Nome: " + cadastrarCachorro.getNome() + "\n" +
+                        "Raça: " + cadastrarCachorro.getRaca() + "\n" +
+                        "Cor: " + cadastrarCachorro.getCor() + "\n" +
+                        "Descrição: " + cadastrarCachorro.getDescricao(),
+                Toast.LENGTH_LONG
+        ).show();
+
+
+    }
+
+    private void limparCampos(){
+            editNome.setText("");
+            editRaca.setText("");
+            editCor.setText("");
+            editDescricao.setText("");
+            editNome.requestFocus();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_cadastro);
+
+
+
+        selectElements();
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        btnCadastro.setOnClickListener(v -> {
+
+
+            verificaCampos();
+
+            limparCampos();
+
+
+
+
+
+        });
+
+
+
+
+
     }
 }
