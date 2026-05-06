eb483bbf55a28e60c20d7c72d306c8302f78c384
vinicius_porto <vinisepo@hotmail.com>
2026-05-06 19:58:58 -0300

    feat: implementa Splashscreen com redirecionamento para MainActivity e ajustes de layout

--- app/src/main/java/com/caesperdidos/app/Splash.java
+++ app/src/main/java/com/caesperdidos/app/Splash.java
@@ -1,7 +1,8 @@
 package com.caesperdidos.app;
 
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.os.Handler;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -15,10 +16,30 @@ public class Splash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
+
+        iniciarSplash();
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    private void iniciarSplash() {
+
+        new Handler().postDelayed(() -> {
+
+            Intent intent = new Intent(Splash.this, MainActivity.class);
+            startActivity(intent);
+
+            finish();
+
+        }, 2000);
+
+    }
+
+
+
 }
