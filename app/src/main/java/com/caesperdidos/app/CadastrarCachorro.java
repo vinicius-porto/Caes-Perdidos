40363cca599f8f90fa3ca794e13b915fd5f9584e
vinicius_porto <vinisepo@hotmail.com>
2026-05-27 23:32:40 -0300

    chore: ajusta a estrutura do arquivo CadastrarCachorro

--- app/src/main/java/com/caesperdidos/app/CadastrarCachorro.java
+++ app/src/main/java/com/caesperdidos/app/CadastrarCachorro.java
@@ -1,52 +1,37 @@
 package com.caesperdidos.app;
 
 public class CadastrarCachorro {
-
-
     private String nome;
     private String raca;
     private String cor;
-
     private String descricao;
-
     private String telefone;
 
+
+    //getters
+    public String getNome() {return nome;}
+    public String getRaca() {return raca;}
+    public String getCor() {return cor;}
+    public String getDescricao() {return descricao;}
     public String getTelefone() { return  telefone; }
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
+    //setters 
+    public void setNome(String nome) {this.nome = nome;}
+    public void setRaca(String raca) {this.raca = raca;}
+    public void setCor(String cor) {this.cor = cor;}
+    public void setDescricao(String descricao) {this.descricao = descricao;}
+    public void setTelefone(String telefone) {this.telefone = telefone;}
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
-    public String getNome() {
-        return nome;
-    }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
-    public String getRaca() {
-        return raca;
-    }
 
-    public void setRaca(String raca) {
-        this.raca = raca;
-    }
 
-    public String getCor() {
-        return cor;
-    }
 
-    public void setCor(String cor) {
-        this.cor = cor;
-    }
+
+
+
+
 
 
 
