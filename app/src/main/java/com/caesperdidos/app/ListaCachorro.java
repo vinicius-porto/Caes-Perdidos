4910b86f25fdef8d1810ab26ccfe9cf6445bce2b
vinicius_porto <vinisepo@hotmail.com>
2026-06-08 21:17:00 -0300

    feat: adiciona os dados para a ListaCachorro

--- app/src/main/java/com/caesperdidos/app/ListaCachorro.java
+++ app/src/main/java/com/caesperdidos/app/ListaCachorro.java
@@ -1,6 +1,8 @@
 package com.caesperdidos.app;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+
+
 public class ListaCachorro extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    private ListView listView;
+    private ArrayList<String> lista;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lista_cachorro);
@@ -20,5 +39,58 @@ public class ListaCachorro extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        listView = findViewById(R.id.listViewCachorros);
+        database = FirebaseDatabase.getInstance();
+        lista = new ArrayList<>();
+
+        adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                lista
+        );
+
+        listView.setAdapter(adapter);
+        carregarDados();
     }
+
+    private void carregarDados() {
+
+        database.getReference("cachorros")
+                .addValueEventListener(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        atualizarLista(snapshot);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
+    }
+
+    private void atualizarLista(DataSnapshot snapshot) {
+
+        lista.clear();
+
+        for (DataSnapshot dados : snapshot.getChildren()) {
+
+            CadastrarCachorro cachorro = dados.getValue(CadastrarCachorro.class);
+
+            if (cachorro != null) {
+                lista.add(formatarCachorro(cachorro));
+            }
+        }
+
+        adapter.notifyDataSetChanged();
+    }
+
+    private String formatarCachorro(CadastrarCachorro cadastrarcachorro)
+    {
+        return "Nome: " + cadastrarcachorro.getNome() + "\n"
+                + "Raça: " + cadastrarcachorro.getRaca() + "\n"
+                + "Cor: " + cadastrarcachorro.getCor() + "\n"
+                + "Tutor: " + cadastrarcachorro.getTutor();
+    }
+
+
 }
